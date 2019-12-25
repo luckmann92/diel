3cd7cd2240766fbd893234f0abac475773261977
genjin145 <genjin145@gmail.com>
2019-12-25 12:09:43 +0500

    Добавить лого, как в макете

--- local/templates/diel/views/modules/sidebar.php
+++ local/templates/diel/views/modules/sidebar.php
@@ -5,6 +5,11 @@
 ?>
 <nav class="flow-menu-nav">
     <ul class="flow-menu">
+        <li class="flow-menu__item flow-menu__logo">
+            <a href="/">
+                <?= GetContentSvgIcon('logo') ?>
+            </a>
+        </li>
         <li class="flow-menu__item">
             <button class="main-menu-button flow-button-menu button-menu"></button>
         </li>
