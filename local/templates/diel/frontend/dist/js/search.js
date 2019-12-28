f11ee7899d439301cca14ea898aca647b67c92f4
genjin145 <genjin145@gmail.com>
2019-12-28 14:18:00 +0500

    Добавлен скрипт для поиска

--- local/templates/diel/frontend/dist/js/search.js
+++ local/templates/diel/frontend/dist/js/search.js
@@ -10,6 +10,28 @@ if (document.querySelector(".main-search__input")) {
     this.parentElement.classList.remove("main-search--focus");
     this.placeholder = "ПОИСК";
   });
+}
 
+if (document.querySelector(".search-section__title")) {
+  let input = document.querySelector(".search-section__title");
 
+  let span = document.createElement("span");
+
+  span.className = "section-title search-section__span";
+
+  input.parentElement.parentElement.insertAdjacentElement("afterEnd", span);
+
+  let len = input.value.length;
+
+  span.textContent = input.value;
+  input.title = input.value;
+
+  for (let i = 1; i <= len; i++) {
+    span.textContent = input.value.slice(0, -i);
+    
+    if (span.offsetWidth < input.offsetWidth) {
+      input.value = span.textContent.slice(0, -3) + "...";
+      break;
+    }  
+  }
 }
