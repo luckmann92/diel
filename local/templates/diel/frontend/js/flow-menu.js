ecc12474774821f5b8614d1e5cfee52da54d5a06
Alexander Goroshnikov <rogsc@mail.ru>
2020-01-15 22:44:45 +0500

    fix scroll titles

--- local/templates/diel/frontend/js/flow-menu.js
+++ local/templates/diel/frontend/js/flow-menu.js
@@ -1,38 +1,33 @@
-window.addEventListener("scroll", function() {
-  let flowMenu = document.querySelector(".flow-menu-nav");
-  if (pageYOffset > 115) {
-    flowMenu.classList.add("flow-menu-nav--active");
-  } else {
-    flowMenu.classList.remove("flow-menu-nav--active");
-  }
+window.addEventListener("scroll", function () {
+    let flowMenu = document.querySelector(".flow-menu-nav");
+    if (pageYOffset > 115) {
+        flowMenu.classList.add("flow-menu-nav--active");
+    } else {
+        flowMenu.classList.remove("flow-menu-nav--active");
+    }
 });
 
 // scrolling headers
-$(document).ready(function (){
-  let flowMenuText = $('.flow-menu__item-text'),
-      sectionsTitle = $('main .section-title'),
-      mainTitle = flowMenuText.text(),
-      titlesOffset = [];
+$(document).ready(function () {
+    let flowMenuText = $('.flow-menu__item-text'),
+        sectionsTitle = $('main .section-title'),
+        mainTitle = flowMenuText.text(),
+        titlesOffset = [];
 
-  sectionsTitle.each(function (i, e) {
-    titlesOffset[i] = $(e).offset();
-  });
+    sectionsTitle.each(function (i, e) {
+        titlesOffset[i] = $(e).offset();
+    });
 
-  console.log(titlesOffset);
-
-  $(document).scroll(function () {
-    if (pageYOffset < (titlesOffset[0]['top'] - $(window).height() / 2) ||
-        pageYOffset > (titlesOffset[titlesOffset.length - 1]['top'] + 700)) {
-      flowMenuText.text(mainTitle);
-    } else {
-      sectionsTitle.each(function (i, e) {
-        console.log('pageYOffset: ' + pageYOffset);
-        console.log('section ' + i + ': ' + titlesOffset[i]['top']);
-        if (pageYOffset > (titlesOffset[i]['top'] - $(window).height() / 2)) {
-          console.log(i);
-          flowMenuText.text(sectionsTitle.eq(i).text());
+    $(document).scroll(function () {
+        if (pageYOffset < (titlesOffset[0]['top'] - $(window).height() * 0.6) ||
+            pageYOffset > (titlesOffset[titlesOffset.length - 1]['top'] + 700)) {
+            flowMenuText.text(mainTitle);
+        } else {
+            sectionsTitle.each(function (i, e) {
+                if (pageYOffset > (titlesOffset[i]['top'] - $(window).height() / 2)) {
+                    flowMenuText.text(sectionsTitle.eq(i).text());
+                }
+            });
         }
-      });
-    }
-  });
+    });
 });
