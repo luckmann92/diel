3ba656ca435dbf9a40b8ff79e068363bbd9007e4
Alexander Goroshnikov <rogsc@mail.ru>
2020-02-02 15:48:58 +0500

    add transition sliders on mobile

--- local/templates/diel/frontend/js/app.js
+++ local/templates/diel/frontend/js/app.js
@@ -243,23 +243,7 @@ function formValidate(form, reqPhone = true, reqEmail = false) {
 }
 
 $(document).ready(function () {
-
-    if (!sortList.is(e.target) && sortList.has(e.target).length === 0) {
-
-//Тип устройства
-        if(device.desktop()) alert('ПК');
-        if(device.tablet()) alert('Планшет');
-        if(device.mobile()) alert('Смартфон');
-//Ориентация экрана
-        if(device.landscape()) alert('Альбомная (в ширину)');
-        if(device.portrait()) alert('Портретная (в высоту)');
-//ОС устройства
-        if(device.ios()) alert('iOS');
-        if(device.ipad()) alert('ipad');
-        if(device.iphone()) alert('iphone');
-        if(device.ipod()) alert('ipod');
-        if(device.android()) alert('android');
-        if(device.windows()) alert('windows');
-        if(device.blackberry()) alert('blackberry');
-        if(device.fxos()) alert('fxos');
-};
+    if (device.tablet() || device.mobile()) {
+        alert(1);
+    }
+});
